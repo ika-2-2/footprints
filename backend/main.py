b1fb53001766d4194fd1f2b588a0f8bcf0469d7e
ika_eater <219417869+ika-2-2@users.noreply.github.com>
2026-02-19 10:17:59 +0900

    feat: API作成

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from db import test_connection
+from sqlalchemy.orm import Session
+
+from db import test_connection, get_db
+from models import Post
+from schemas import PostCreate, PostOut
 
 app = FastAPI()
 
@@ -12,10 +16,34 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 @app.on_event("startup")
 def on_startup():
     test_connection()
 
+
 @app.get("/")
 def root():
     return {"ok": True}
+
+
+@app.post("/posts", response_model=PostOut)
+def create_post(payload: PostCreate, db:Session = Depends(get_db)):
+    # id存在チェックまだ
+    post = Post(
+        user_id=payload.user_id,
+        body=payload.body,
+        lat=payload.lat,
+        lng=payload.lng,
+    )
+    db.add(post)
+    db.commit()
+    db.refresh(post)
+    return post
+
+@app.get("/posts/{post_id}", response_model=PostOut)
+def read_post(post_id: int, db: Session = Depends(get_db)):
+    post = db.query(Post).filter(Post.id == post_id).first()
+    if post is None:
+        raise HTTPException(status_code=404, detail="Post not found")
+    return post

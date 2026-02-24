2749dd58133c8180de0569b270f178fa84ff0f4c
ika_eater <219417869+ika-2-2@users.noreply.github.com>
2026-02-24 16:30:39 +0900

    fix: 誤字

--- frontend/src/NotifyPage.tsx
+++ frontend/src/NotifyPage.tsx
@@ -2,21 +2,22 @@ import BottomNav from "./BottomNav";
 import type { LoginInfo } from "./App";
 import "./css/common.css";
 
-export default function NotifyPage({onGoTimeline, onGoPost, onGoProfile }: {
+export default function NotifyPage({onGoTimeline, onGoPost, onGoProfile, onGoSearch }: {
   login: LoginInfo;
   onGoTimeline: () => void;
   onGoPost: () => void;
   onGoProfile?: () => void;
+  onGoSearch?: () => void;
 }) {
   return (
     <div className="container">
       <header>
-        <span className="header-logo">通知</span>
+        <span className="header-logo">footprints</span>
       </header>
       <div className="coming-soon">
         <p>準備中です...</p>
       </div>
-      <BottomNav active="notify" onGoTimeline={onGoTimeline} onGoPost={onGoPost} onGoProfile={onGoProfile} />
+      <BottomNav active="notify" onGoTimeline={onGoTimeline} onGoPost={onGoPost} onGoProfile={onGoProfile} onGoSearch={onGoSearch} />
     </div>
   );
 }
--- frontend/src/SearchPage.tsx
+++ frontend/src/SearchPage.tsx
@@ -2,21 +2,22 @@ import BottomNav from "./BottomNav";
 import type { LoginInfo } from "./App";
 import "./css/common.css";
 
-export default function SearchPage({onGoTimeline, onGoPost, onGoProfile }: {
+export default function SearchPage({onGoTimeline, onGoPost, onGoProfile, onGoNotify }: {
   login: LoginInfo;
   onGoTimeline: () => void;
   onGoPost: () => void;
   onGoProfile?: () => void;
+  onGoNotify?: () => void;
 }) {
   return (
     <div className="container">
       <header>
-        <span className="header-logo">検索</span>
+        <span className="header-logo">footprints</span>
       </header>
       <div className="junbi-tyu">
         <p>準備中です...</p>
       </div>
-      <BottomNav active="search" onGoTimeline={onGoTimeline} onGoPost={onGoPost} onGoProfile={onGoProfile} />
+      <BottomNav active="search" onGoTimeline={onGoTimeline} onGoPost={onGoPost} onGoProfile={onGoProfile} onGoNotify={onGoNotify} />
     </div>
   );
 }
